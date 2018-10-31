db474f7d3c144c3af4bae77b1d870e913b317f18
Lumar <contactmarisalu@gmail.com>
2018-10-30 22:46:20 -0700

    i didn't understand that build was different from deploy.
    ...thought it was up ages ago...

---
+++ precache-manifest.cf9d2d08c3db7d8b7aca7cd2a904a2f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "67cab17e642ab111532f6c107a6fc780",
+    "url": "/simpleReact/static/media/2018.67cab17e.png"
+  },
+  {
+    "revision": "371ed9486081aea64a6eec4b7d192837",
+    "url": "/simpleReact/static/media/2017.371ed948.png"
+  },
+  {
+    "revision": "f2c4d0a7f59a07ac5d9e52e7044f716f",
+    "url": "/simpleReact/static/media/2016.f2c4d0a7.png"
+  },
+  {
+    "revision": "f2c4d0a7f59a07ac5d9e52e7044f716f",
+    "url": "/simpleReact/static/media/2015.f2c4d0a7.png"
+  },
+  {
+    "revision": "e09f6979a48778a0d637",
+    "url": "/simpleReact/static/js/runtime~main.e09f6979.js"
+  },
+  {
+    "revision": "7b65eca5a3b27ceb442b",
+    "url": "/simpleReact/static/js/main.7b65eca5.chunk.js"
+  },
+  {
+    "revision": "1cd938f0aee440b73899",
+    "url": "/simpleReact/static/js/1.1cd938f0.chunk.js"
+  },
+  {
+    "revision": "7b65eca5a3b27ceb442b",
+    "url": "/simpleReact/static/css/main.cdc11551.chunk.css"
+  },
+  {
+    "revision": "a464a882903b17671a78496896b36a0d",
+    "url": "/simpleReact/index.html"
+  }
+];
